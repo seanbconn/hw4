3ce86f643ec18f4afa87e8ba0e7386f7719b18b7
Sean Connolly <sean.connolly@northwestern.edu>
2024-03-04 23:56:46 +0000

    building places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,9 +13,14 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user !=nil
+     @place = Place.new
+     @place["name"] = params["name"]
+     @place.save
+    else 
+      flash["notice"] = "Login to view this content"
+    end
     redirect_to "/places"
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
-    @user["email"] = params["email"]
-    @user["password"] = BCrypt::Password.create(params["password"])
-    @user.save
-    redirect_to "/login"
+    if User.find_by({ "email" => params["email"] }) == nil
+     @user = User.new
+      @user["first_name"] = params["first_name"]
+      @user["last_name"] = params["last_name"]
+      @user["email"] = params["email"]
+      @user["password"] = BCrypt::Password.create(params["password"])
+      @user.save
+      redirect_to "/login"
+    else
+      flash["notice"] = "Email in use, account unavailable."
+      redirect_to "/users/new"
+
+
   end
 end
