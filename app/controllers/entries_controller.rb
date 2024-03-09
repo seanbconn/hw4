92fe24f3a064ec55ecdb42faba82f82fa0e6feb5
Sean Connolly <sean.connolly@northwestern.edu>
2024-03-09 03:33:51 +0000

    auth done for create place/entries and view place/entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,14 +4,19 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = session["user_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    if User.find_by({"id" => session["user_id"]}) != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = session["user_id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    else
+      flash["notice"] = "Login to create content."
+      redirect_to "/login"
+    end 
   end
 
 end
