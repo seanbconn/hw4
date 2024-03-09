d2fd6720569a5cde34097506817beedd575a7f8c
Sean Connolly <sean.connolly@northwestern.edu>
2024-03-09 01:31:44 +0000

    authenticate in application controller?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "Welcome "
+    @current_user = User.find_by({ "id" => session["user_id"] })
+    puts "Welcome to your Gram"
   end
 end
