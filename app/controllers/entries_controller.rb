43bd131e0febf896c725fc1983a3d5b1fd6ca503
Sean Connolly <sean.connolly@northwestern.edu>
2024-03-12 01:10:43 +0000

    Images now pulling through

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,7 +9,7 @@ class EntriesController < ApplicationController
       @entry = Entry.new
       @entry["title"] = params["title"]
       @entry["description"] = params["description"]
-      @entry.uploaded_image.attach(params["entry"]["uploaded_image"])
+      @entry.uploaded_image.attach(params["uploaded_image"])
       @entry["occurred_on"] = params["occurred_on"]
       @entry["place_id"] = params["place_id"]
       @entry["user_id"] = session["user_id"]
